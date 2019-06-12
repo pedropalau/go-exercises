50b113d6038bb474a8b98a28ce32c796fff21592
Pedro Enrique Palau <pepalauisaac@gmail.com>
2019-06-12 00:16:47 -0500

    Initial creation of the array struct

---
+++ arrays/operations/array.go
@@ -0,0 +1,11 @@
+package main
+
+type array struct {
+	// The `length` of the array.
+	// Represent the total items the array have.
+	count int
+
+	// The elements in the array
+	// @TODO: make this a type-dynamic array
+	items int
+}
---
+++ arrays/operations/main.go
@@ -0,0 +1,7 @@
+package main
+
+import "fmt"
+
+func main() {
+	fmt.Println("Custom array struct created")
+}
